6e8dc01a20497b5a82bda8bf81e9b092fcb90d0b
Catherine Mao <94186644+catherinemao18@users.noreply.github.com>
2021-12-21 01:50:26 +0000

    finished part four

--- part4.py
+++ part4.py
@@ -40,3 +40,14 @@ fizz
 """
 
 # write your code here 
+capillary = int(input("Enter a number: "))
+
+for i in range(1, capillary + 1):
+  if i % 3 == 0:
+    print("fizz")
+  elif i % 5 == 0:
+    print ("buzz")
+  elif i % 15 == 0:
+    print ("fizzbuzz")
+  else:
+    print (i)
